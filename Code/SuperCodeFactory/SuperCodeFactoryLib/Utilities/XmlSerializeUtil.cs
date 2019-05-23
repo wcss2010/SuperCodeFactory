800f08e46def4db2c667c547c2d0a64bb42df8d8
wcss <wcss2020@gmail.com>
2019-05-23 16:22:51 +0800

    。。。

--- Code/SuperCodeFactory/SuperCodeFactoryLib/Utilities/XmlSerializeUtil.cs
+++ Code/SuperCodeFactory/SuperCodeFactoryLib/Utilities/XmlSerializeUtil.cs
@@ -17,7 +17,7 @@ namespace SuperCodeFactoryLib.Utilities
         /// <typeparam name="T">将要序列化或反序列化的类型</typeparam>
         /// <param name="xml">源Xml文本</param>
         /// <returns>反序列化后的实例对象</returns>
-        public static T deserialize<T>(string xml)
+        public static T Deserialize<T>(string xml)
         {
             XmlSerializer xs = new XmlSerializer(typeof(T));
             StringReader sr = new StringReader(xml);
@@ -33,7 +33,7 @@ namespace SuperCodeFactoryLib.Utilities
         /// <typeparam name="T">将要序列化或反序列化的类型</typeparam>
         /// <param name="t">源对象</param>
         /// <returns>序列化后的XML文本</returns>
-        public static string serializer<T>(T t)
+        public static string Serializer<T>(T t)
         {
             XmlSerializerNamespaces xsn = new XmlSerializerNamespaces();
             xsn.Add(string.Empty, string.Empty);
