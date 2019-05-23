f6537aaf141b2ffddb1f6951aff5ad55fbb5dae9
wcss <wcss2020@gmail.com>
2019-05-23 19:44:25 +0800

    。。。

--- Code/SuperCodeFactory/SuperCodeFactory/MainForm.Designer.cs
+++ Code/SuperCodeFactory/SuperCodeFactory/MainForm.Designer.cs
@@ -28,9 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.Name = "MainForm";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- Code/SuperCodeFactory/SuperCodeFactory/MainForm.cs
+++ Code/SuperCodeFactory/SuperCodeFactory/MainForm.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using SuperCodeFactoryUILib.Forms;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace SuperCodeFactory
@@ -14,6 +16,46 @@ namespace SuperCodeFactory
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            CircleProgressBarDialog bar = new CircleProgressBarDialog();
+            bar.BackColor = Color.Gray;
+            bar.ProgressBarColor = Color.MediumPurple;
+            bar.ProgressBarBackColor = Color.Gray;
+            bar.ProgressBarFontColor = Color.Red;
+            bar.DefaultLabelForeColor = Color.Blue;
+            bar.DefaultLabelFont = new System.Drawing.Font("宋体", 20);
+
+            bar.ShowProgressBarDialog((obj, args) =>
+            {
+                for (var currNum = 0; currNum <= 99; currNum++)
+                {
+                    bar.ProgressUpdate(currNum, 99);//处理过程中改变百分比
+                    Thread.Sleep(500);//耗时操作
+
+                    if (currNum == 20)
+                    {
+                        bar.ProgressUpdate("AAAAAAAAAAAAAAAA");
+                    }
+
+                    if (currNum == 30)
+                    {
+                        bar.ProgressUpdate("BBBBBBBBBBBBBBB");
+                    }
+
+                    if (currNum == 40)
+                    {
+                        bar.ProgressUpdate("CCCCCCCCCCCCCC");
+                    }
+
+                    if (currNum == 50)
+                    {
+                        bar.CloseProgressBarDialog();
+                    }
+                }
+            }, "AAA");
         }
     }
 }
