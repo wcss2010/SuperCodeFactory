143e0a8fdab863c41ffeefdfe14bf8e86bb0da81
wcss <wcss2020@gmail.com>
2019-05-24 14:25:20 +0800

    。。。

---
+++ Code/SuperCodeFactory/SuperCodeFactory/DBSchema/Database/Entity/DbConn.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SuperCodeFactory.DBSchema.Db
+{
+    /// <summary>
+    /// 数据库连接
+    /// </summary>
+    public class DbConn
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ConnectionString { get; set; }
+        public string ProviderName { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
