be404f5238f0f9daf48425c1a03c4348855cb8a5
wcss <wcss2020@gmail.com>
2019-11-29 10:02:23 +0800

    ...

--- Code/SuperCodeFactory/SuperCodeFactory/MainForm.Designer.cs
+++ Code/SuperCodeFactory/SuperCodeFactory/MainForm.Designer.cs
@@ -338,7 +338,7 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "SuperCodeFactory V1.1";
+            this.Text = "SuperCodeFactory V1.2";
             this.Load += new System.EventHandler(this.MainForm_Load);
             this.plTopBar.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
